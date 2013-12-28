95debdb997c27d5a646e39d0927a5fb87dce4a15
Teja Chiluvuri <tejachil@vt.edu>
2013-12-28 11:36:09 -0500

    Got rid of the host address.

--- udp-control-server.js
+++ udp-control-server.js
@@ -1,4 +1,3 @@
-var HOST = '127.0.0.1';
 var SERVER_PORT = 33333;
 var CLIENT_PORT = 33334;
 
@@ -15,4 +14,4 @@ server.on('message', function (message, remote) {
 	// server.send(message, 0, message.length, CLIENT_PORT, remote.address); // echos message
 });
 
-server.bind(SERVER_PORT, HOST);
+server.bind(SERVER_PORT);
