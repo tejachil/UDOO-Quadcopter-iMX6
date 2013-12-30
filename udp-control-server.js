a608e0121763bfebf24d7c1aa264277bad91f5b0
Teja Chiluvuri <tejachil@vt.edu>
2013-12-30 11:21:38 -0500

    Added framework for second client port for AHRS.

--- udp-control-server.js
+++ udp-control-server.js
@@ -1,6 +1,7 @@
-var CLIENT_HOST = '127.0.0.1';
 var SERVER_PORT = 33333;
-var CLIENT_PORT = 33334;
+var CLIENT_HOST = '192.168.1.8';
+var CLIENT_CONTROL_PORT = 33334;
+var CLIENT_AHRS_PORT = 33335;
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
@@ -12,7 +13,7 @@ server.on('listening', function () {
 
 server.on('message', function (message, remote) {
 	console.log(remote.address + ':' + remote.port +' - ' + message);
-	// server.send(message, 0, message.length, CLIENT_PORT, remote.address); // echos message
+	// server.send(message, 0, message.length, CLIENT_CONTROL_PORT, remote.address); // echos message
 });
 
 server.bind(SERVER_PORT);
