489d9394ce267dd4143be37e52d7ee8c3e6e48c7
Teja Chiluvuri <tejachil@vt.edu>
2013-12-30 13:53:01 -0500

    Added the parser for the serial port to read line.

--- udp-control-server.js
+++ udp-control-server.js
@@ -6,10 +6,12 @@ var CLIENT_AHRS_PORT = 33335;
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyUSB0", {
-  baudrate: 57600
+var com = require("serialport")
+var serialPort = new com.SerialPort("/dev/ttyUSB0", {
+  baudrate: 57600,
+  parser: com.parsers.readline('\n')
 }, false);
+
 
 server.on('listening', function () {
 	var address = server.address();
@@ -17,16 +19,23 @@ server.on('listening', function () {
 });
 
 server.on('message', function (message, remote) {
+	if(remote.port == CLIENT_AHRS_PORT){
+		console.log("AHRS Command: " + message);
+		serialPort.write(message);
+	}
+	else
 	console.log(remote.address + ':' + remote.port +' - ' + message);
-	// server.send(message, 0, message.length, CLIENT_CONTROL_PORT, remote.address); // echos message
+	server.send(message, 0, message.length, CLIENT_CONTROL_PORT, remote.address); // echos message
 });
 
 serialPort.open(function () {
   console.log('open');
-  serialPort.on('data', function(data) {
-    //console.log('data received: ' + data);
-    server.send(data, 0, data.length, CLIENT_AHRS_PORT, CLIENT_HOST); // echos message
-  });
+});
+
+serialPort.on('data', function(data) {
+	//console.log('data received: ' + data);
+	var dataBuf = new Buffer(data);
+	server.send(dataBuf, 0, dataBuf.length, CLIENT_AHRS_PORT, CLIENT_HOST); // echos message
 });
 
 server.bind(SERVER_PORT);
