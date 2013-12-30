764c2480e24cfd3872f476676d30f84e957f4235
Teja Chiluvuri <tejachil@vt.edu>
2013-12-30 12:14:27 -0500

    Serial communication implemented on the node server.

--- udp-control-server.js
+++ udp-control-server.js
@@ -6,6 +6,11 @@ var CLIENT_AHRS_PORT = 33335;
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyUSB0", {
+  baudrate: 57600
+}, false);
+
 server.on('listening', function () {
 	var address = server.address();
 	console.log('UDP Server listening on ' + address.address + ":" + address.port);
@@ -14,6 +19,14 @@ server.on('listening', function () {
 server.on('message', function (message, remote) {
 	console.log(remote.address + ':' + remote.port +' - ' + message);
 	// server.send(message, 0, message.length, CLIENT_CONTROL_PORT, remote.address); // echos message
+});
+
+serialPort.open(function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    //console.log('data received: ' + data);
+    server.send(data, 0, data.length, CLIENT_AHRS_PORT, CLIENT_HOST); // echos message
+  });
 });
 
 server.bind(SERVER_PORT);
