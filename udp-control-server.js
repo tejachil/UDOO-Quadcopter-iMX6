4a6975f2654c947a57ca836ab9aa232ede51414b
Teja Chiluvuri <tejachil@vt.edu>
2014-01-03 15:19:03 -0500

    Updated the serial port to that on the UDOO board.

--- udp-control-server.js
+++ udp-control-server.js
@@ -2,12 +2,13 @@ var SERVER_PORT = 33333;
 var CLIENT_HOST = '192.168.1.8';
 var CLIENT_CONTROL_PORT = 33334;
 var CLIENT_AHRS_PORT = 33335;
+var SERIAL_PORT = "/dev/ttymxc3";
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
 var com = require("serialport")
-var serialPort = new com.SerialPort("/dev/ttyUSB0", {
+var serialPort = new com.SerialPort(SERIAL_PORT, {
   baudrate: 57600,
   parser: com.parsers.readline('\n')
 }, false);
@@ -29,7 +30,7 @@ server.on('message', function (message, remote) {
 });
 
 serialPort.open(function () {
-  console.log('open');
+  console.log('Serial Port open on ' + SERIAL_PORT);
 });
 
 serialPort.on('data', function(data) {
